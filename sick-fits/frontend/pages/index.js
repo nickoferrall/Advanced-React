a8f3e869282f8179fd398024bee4be0df72a3cf3
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-02-09 19:25:41 -0500

    Completed styling - beginning lecture 10 on backend

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import Link from 'next/link';
+
+class index extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Heyyy</h1>
+      </div>
+    );
+  }
+}
+
+export default index;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Sell = props => {
+  return (
+    <div>
+      <h3>Selling!</h3>
+    </div>
+  );
+};
+
+export default Sell;
