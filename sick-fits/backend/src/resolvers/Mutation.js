5437d7e1e312032135ba963a55e329fd48b2bec8
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-03-02 17:43:53 -0500

    Setting up Apollo Client with React complete

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,10 +1,21 @@
-const mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
   }
+  //   createDog(parent, args, ctx, info) {
+  //     global.dogs = global.dogs || [];
+  //     const newDog = { name: args.name };
+  //     global.dogs.push(newDog);
+  //     return newDog;
+  // }
 };
 
-module.exports = mutations;
+module.exports = Mutations;
